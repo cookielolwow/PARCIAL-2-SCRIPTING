00aed48c9d32664fa7bc29c1d7e7ca2a1cf5e971
Ayepes2402 <andrea.yepese@upb.edu.co>
2026-03-16 20:41:10 -0500

    Add Comprar method for item purchases
    Implement purchasing logic in ServicioCompra class.

--- parcial/ServicioCompra.cs
+++ parcial/ServicioCompra.cs
@@ -1,8 +1,50 @@
-﻿using System;
+using System.Collections.Generic;
 
 public class ServicioCompra
 {
-	public ServicioCompra()
-	{
-	}
+    public static bool Comprar(Jugador jugador, Tienda tienda, Dictionary<Item, int> itemsAComprar)
+    {
+        if (jugador == null  tienda == null 
+ itemsAComprar == null)
+            return false;
+
+        decimal costoTotal = 0;
+
+        foreach (var entry in itemsAComprar)
+        {
+            var item = entry.Key;
+            var cantidad = entry.Value;
+
+            if (cantidad <= 0)
+                return false;
+
+            if (!tienda.TieneStock(item, cantidad))
+                return false;
+
+            costoTotal += item.Precio * cantidad;
+        }
+
+        if (!jugador.PuedePagar(costoTotal))
+            return false;
+
+ 
+        if (!jugador.GastarOro(costoTotal))
+            return false;
+
+        foreach (var entry in itemsAComprar)
+        {
+            var item = entry.Key;
+            var cantidad = entry.Value;
+
+            if (!tienda.ReducirStock(item, cantidad))
+                return false;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                jugador.AgregarItem(item);
+            }
+        }
+
+        return true;
+    }
 }
